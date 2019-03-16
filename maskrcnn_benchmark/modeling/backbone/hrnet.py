d6f9bfe305eaf0af41478349a5c58004392189d0
shsjxzh <1927045093@qq.com>
2019-03-16 22:19:41 +0800

    change

--- maskrcnn_benchmark/modeling/backbone/hrnet.py
+++ maskrcnn_benchmark/modeling/backbone/hrnet.py
@@ -278,6 +278,7 @@ class PoseHighResolutionNet(nn.Module):
 
     def __init__(self, cfg, **kwargs):
         self.inplanes = 64
+        self.out_channels = cfg.MODEL.HRNET.DETAIL_EXTRA.STAGE4[-1]
         extra = cfg.MODEL.HRNET.DETAIL_EXTRA
         super(PoseHighResolutionNet, self).__init__()
 
